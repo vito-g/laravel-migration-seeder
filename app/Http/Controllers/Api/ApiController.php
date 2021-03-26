656482d7aa6f663d58f051a37e56a82c6e4016a6
Vito Gentile <gentilevito1076@gmail.com>
2021-03-26 18:33:45 +0100

    Restituito il primo elemento della tabella attraverso API

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -11,7 +11,8 @@ class ApiController extends Controller
     public function getFirstCoffee() {
       //Creare un Api che restituisce il primo caffè della tabella del database coffee
       // dd('prova');
-      $firstCoffee = Coffee::all();
+      // $firstCoffee = Coffee::all();//Salvo tutta la tab coffees in $firstCoffee
+      $firstCoffee = Coffee::orderBy('id', 'asc')->first();//Ritorno solo il primo caffè della tab del mio database
       return response()->json($firstCoffee);
     }
 }
