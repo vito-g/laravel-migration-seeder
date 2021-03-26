64d772ae2f9f978956361fc994afbf3a28844392
Vito Gentile <gentilevito1076@gmail.com>
2021-03-26 17:40:50 +0100

    New Seeder for Multiple Attributes Adding  + Use Model & Faker

---
+++ database/seeds/MoreCoffeesTableSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Coffee;
+use Faker\Generator as Faker;
+
+class MoreCoffeesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
