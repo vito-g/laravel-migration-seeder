c6ed33ea0d79c0d5bb23f5c87592e90aab18b332
Vito Gentile <gentilevito1076@gmail.com>
2021-03-26 18:29:41 +0100

    Ritorno tutto il json

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -4,11 +4,14 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Coffee;
 
 class ApiController extends Controller
 {
     public function getFirstCoffee() {
       //Creare un Api che restituisce il primo caffè della tabella del database coffee
-      dd('prova');
+      // dd('prova');
+      $firstCoffee = Coffee::all();
+      return response()->json($firstCoffee);
     }
 }
